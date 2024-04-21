345aa6515c4214c4aec6eb3d18206e8da0fc3cfd
Gal <64677161+galzorigo@users.noreply.github.com>
2024-04-21 16:29:24 +0800

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from './components/Button';
-import Card from './components/Card';
 import NavSingle from './components/NavSingle';
 
 import { Home, Bus, Settings, Search } from 'lucide-react'
