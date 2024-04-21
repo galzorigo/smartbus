6211f11714943908545f46460bbb1e55c3e167aa
Gal <64677161+galzorigo@users.noreply.github.com>
2024-04-21 22:22:24 +0800

    shrinking effect on search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 export default function SearchBar({ icon, link }) {
     return (
-        <div className="flex items-center gap-[12px] bg-gray-100 p-[12px] rounded-[12px] h-[44px]">
+        <div className="flex items-center gap-[12px] bg-gray-100 p-[12px] rounded-[12px] h-[44px] duration-150 ease-in-out active:scale-95">
             <div className="flex items-center justify-center h-[20px] w-[20px] text-gray-400 rounded-full">
                 {icon}
             </div>
