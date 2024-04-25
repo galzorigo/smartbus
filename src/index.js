f10a5e84308cb3205aaba630335114d4a67016e6
Gal <64677161+galzorigo@users.noreply.github.com>
2024-04-25 13:42:33 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-//import App from './Components';
+import App from './Components';
 //import HomeScreen from './HomeScreen';
-import SeeBus from './SeeBus';
+//import SeeBus from './SeeBus';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <SeeBus />
+  <App />
 );
